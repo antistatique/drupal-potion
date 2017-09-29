0c638cf92351628dc42cb209bdd6463a47270436
Gilles Doge <gilles@antistatique.net>
2017-09-29 17:10:27 +0200

    Add Extractor Tests but there are not working right now

---
+++ tests/src/Kernel/Extractor/TwigExtractorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\potion\tests\Kernel\Extractor;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\potion\Extractor\TwigExtractor;
+
+/**
+ * Tests Twig Extractor
+ *
+ * @group potion
+ */
+class TwigExtractorTest extends KernelTestBase  {
+
+  public static $modules = [
+    'locale',
+    'language'
+  ];
+
+  /**
+   * @var \Twig_Environment
+   *  Twig environment
+   */
+  private $twig;
+
+  protected function setUp() {
+    parent::setUp();
+    $this->twig = $this->container->get('twig');
+  }
+
+  public function testExtract() {
+    $expected = [
+      'foo.t',
+      //'foo.trans',
+      'foo.bar @baz',
+      'foo trans block @complex_object.foo',
+      'foo trans block @complex_object.foo and @count',
+      'foo trans with placeholder @string', // @TODO: is this is expected behavior ? no more '!string'?
+      "\nfoo trans with raw @string and space\n",
+      'Hello star.',
+      'Hello @count stars.',
+      'foo.file2',
+    ];
+
+    var_dump($this->twig->getExtensions());
+
+    $extractor = new TwigExtractor($this->twig);
+    $path = __DIR__ . '/../../../modules/potion_test/templates';
+
+    $actual = $extractor->extract($path);
+
+    $this->assertSame($expected, $actual);
+  }
+}
