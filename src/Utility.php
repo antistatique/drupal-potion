4d305863fdba031056b7cf9b87168b0fe9f3c1ce
Kevin Wenger <wenger.kev@gmail.com>
2018-05-02 19:56:08 +0200

    add Utility class & unit tests as coverage

---
+++ src/Utility.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\potion;
+
+use Drupal\Core\Language\LanguageManagerInterface;
+
+/**
+ * Contains utility methods for the Potion module.
+ */
+class Utility {
+  const GETTEXT_VERSION = '0.19.8.1';
+
+  /**
+   * The language Manager.
+   *
+   * @var \Drupal\language\ConfigurableLanguageManager
+   */
+  protected $languageManager;
+
+  /**
+   * Class constructor.
+   *
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   */
+  public function __construct(LanguageManagerInterface $language_manager) {
+    $this->languageManager = $language_manager;
+  }
+
+  /**
+   * Check if the given langcode is installed & enabled.
+   *
+   * @param string $langcode
+   *   The langcode to test.
+   *
+   * @return bool
+   *   TRUE if the given langcode exists, FALSE otherwise.
+   */
+  public function isLangcodeEnabled($langcode) {
+    $languages = $this->languageManager->getLanguages();
+    return isset($languages[$langcode]);
+  }
+
+  /**
+   * Check if the given file path is a valid .po file.
+   *
+   * @param string $src
+   *   The file to validate.
+   *
+   * @return bool
+   *   TRUE if the given file is valid, FALSE otherwise.
+   */
+  public function isValidPo($src) {
+    $msgfmt = __DIR__ . '/../scripts/gettext/' . self::GETTEXT_VERSION . '/bin/msgfmt';
+
+    if (!is_file($src)) {
+      return FALSE;
+    }
+
+    exec($msgfmt . ' --check ' . $src . ' 2>&1', $output, $result);
+    if ($result > 0) {
+      return FALSE;
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * Determines whether this PHP process is running on the command line.
+   *
+   * @return bool
+   *   TRUE if this PHP process is running via CLI, FALSE otherwise.
+   */
+  public static function isRunningInCli() {
+    return php_sapi_name() === 'cli';
+  }
+
+}
---
+++ tests/src/Unit/UtilityTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Drupal\Tests\extranet_automatic_import\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\potion\Utility;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Language\Language;
+
+/**
+ * @coversDefaultClass \Drupal\potion\Utility
+ * @group potion
+ * @group potion_unit
+ * @group potion_unit_utility
+ */
+class UtilityTest extends UnitTestCase {
+
+  /**
+   * The Utility service of Potion.
+   *
+   * @var \Drupal\potion\Utility
+   */
+  protected $utility;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $en = new Language([
+      'id'        => 'en',
+      'name'      => 'English',
+      'direction' => Language::DIRECTION_LTR,
+      'weight'    => 0,
+      'locked'    => FALSE,
+    ]);
+
+    $fr = new Language([
+      'id'        => 'fr',
+      'name'      => 'French',
+      'direction' => Language::DIRECTION_LTR,
+      'weight'    => 1,
+      'locked'    => FALSE,
+    ]);
+
+    /** @var \Drupal\Core\Language\LanguageManagerInterface|\Prophecy\Prophecy\ProphecyInterface $language_manager */
+    $language_manager = $this->prophesize(LanguageManagerInterface::class);
+    $this->utility = new Utility($language_manager->reveal());
+    $language_manager->getLanguages()
+      ->willReturn(['en' => $en, 'fr' => $fr]);
+  }
+
+  /**
+   * @covers \Drupal\potion\Utility::isValidPo
+   * @dataProvider getTestIsValidPo
+   */
+  public function testIsValidPo($filepath, $expected) {
+    $result = $this->utility->isValidPo($filepath);
+    $this->assertEquals($result, $expected);
+  }
+
+  /**
+   * Provider of testIsValidPo.
+   *
+   * @return array
+   *   Return an array of arrays.
+   */
+  public function getTestIsValidPo() {
+    $dir = __DIR__ . '/../../modules/potion_test/assets';
+    $dir_malformed = $dir . '/malformed';
+
+    return [
+      [$dir, FALSE],
+      [$dir_malformed, FALSE],
+      [$dir . '/en.po', FALSE],
+      [$dir . '/fr.po', TRUE],
+      [$dir . '/de.po', TRUE],
+      [$dir_malformed . '/missing-msgid.po', FALSE],
+      [$dir_malformed . '/missing-msgstr.po', FALSE],
+      [$dir_malformed . '/no-header.po', FALSE],
+      [$dir_malformed . '/quote.po', FALSE],
+    ];
+  }
+
+  /**
+   * @covers \Drupal\potion\Utility::isLangcodeEnabled
+   * @dataProvider getTestIsLangcodeEnabled
+   */
+  public function testIsLangcodeEnabled($langcode, $expected) {
+    $result = $this->utility->isLangcodeEnabled($langcode);
+    $this->assertEquals($result, $expected);
+  }
+
+  /**
+   * Provider of testIsLangcodeEnabled.
+   *
+   * @return array
+   *   Return an array of arrays.
+   */
+  public function getTestIsLangcodeEnabled() {
+    return [
+      ['fr', TRUE],
+      ['fr-ch', FALSE],
+      ['de', FALSE],
+      ['de-ch', FALSE],
+      ['en', TRUE],
+    ];
+  }
+
+}
