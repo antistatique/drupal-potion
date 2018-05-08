357f2809bca3ca52ba76061c1e1daf03cff69835
Kevin Wenger <wenger.kev@gmail.com>
2018-05-08 08:06:39 +0200

    add service to import translations

---
+++ src/TranslationsImport.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\potion;
+
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\locale\Gettext;
+use Drupal\potion\Exception\PotionException;
+
+/**
+ * Translations Importations.
+ */
+class TranslationsImport {
+
+  /**
+   * The Utility service of Potion.
+   *
+   * @var \Drupal\potion\Utility
+   */
+  protected $utility;
+
+  /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Class constructor.
+   *
+   * @param \Drupal\potion\Utility $utility
+   *   Utility methods for Potion.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $utility
+   *   The module handler service.
+   */
+  public function __construct(Utility $utility, ModuleHandlerInterface $module_handler) {
+    $this->utility = $utility;
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * Translation(s) exportation from database to .po file.
+   *
+   * @param string $langcode
+   *   Language code of the language being written to the database.
+   * @param string $source
+   *   The .po file's path.
+   *
+   * @return array
+   *   Report array as defined in Drupal\locale\PoDatabaseWriter.
+   */
+  public function importFromFile($langcode, $source, $options = []) {
+    // Check for existing & enabled langcode.
+    if (!$this->utility->isLangcodeEnabled($langcode)) {
+      throw PotionException::invalidLangcode($langcode);
+    }
+
+    // Check for existing source with valid .po content.
+    if (!$this->utility->isValidPo($source)) {
+      throw PotionException::invalidPo($source);
+    }
+
+    // Load Drupal 8 Core local global functions.
+    $this->moduleHandler->loadInclude('locale', 'translation.inc');
+    $this->moduleHandler->loadInclude('locale', 'bulk.inc');
+
+    $customized = LOCALE_NOT_CUSTOMIZED;
+    if (isset($options['customized']) && $options['customized']) {
+      $customized = LOCALE_CUSTOMIZED;
+    }
+
+    $overwrite = FALSE;
+    if (isset($options['overwrite']) && $options['overwrite']) {
+      $overwrite = (bool) $options['overwrite'];
+    }
+
+    $options = array_merge(_locale_translation_default_update_options(), [
+      'customized'        => $customized,
+      'overwrite_options' => [
+        'not_customized' => !$customized && $overwrite ? TRUE : FALSE,
+        'customized'     => $customized && $overwrite ? TRUE : FALSE,
+      ],
+    ]);
+
+    // Create a valid file class for Gettext::fileToDatabase.
+    $file            = new \stdClass();
+    $file->filename  = drupal_basename($source);
+    $file->uri       = $source;
+    $file->langcode  = $langcode;
+    $file->timestamp = filemtime($source);
+
+    return Gettext::fileToDatabase($file, $options);
+  }
+
+}
