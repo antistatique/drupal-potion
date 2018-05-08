2b6d46e403ec3d402b751af2d52280a33ff69588
Kevin Wenger <wenger.kev@gmail.com>
2018-05-08 07:53:04 +0200

    add GettextException handler

---
+++ src/Exception/GettextException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\potion\Exception;
+
+/**
+ * Represents an exception that occurred in some part of Gettext wrappers.
+ */
+class GettextException extends PotionException {
+}
