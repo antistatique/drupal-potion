88a920092f98c3009deba95e6ecf4c4ec4d2537e
Kevin Wenger <wenger.kev@gmail.com>
2018-05-09 10:16:38 +0200

    refactoring GettextWrapper to be a service & use /Users/wengerk/.nvm/versions/node/v9.11.1/bin:/Users/wengerk/.rbenv/shims:/Users/wengerk/.rbenv/shims:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/wengerk/.composer/vendor/bin:/usr/local/Cellar/mysql/5.7.13/bin env or custom config potion path to access gettext binaries

--- tests/src/Unit/UtilityTest.php
+++ tests/src/Unit/UtilityTest.php
@@ -4,8 +4,11 @@ namespace Drupal\Tests\potion\Unit;
 
 use Drupal\Tests\UnitTestCase;
 use Drupal\potion\Utility;
+use Drupal\potion\GettextWrapper;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Language\Language;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
 
 /**
  * @coversDefaultClass \Drupal\potion\Utility
@@ -44,9 +47,22 @@ class UtilityTest extends UnitTestCase {
       'locked'    => FALSE,
     ]);
 
+    /** @var \Drupal\Core\Config\ConfigFactoryInterface|\Prophecy\Prophecy\ProphecyInterface $config_factory */
+    $config_factory = $this->prophesize(ConfigFactoryInterface::class);
+    /** @var \Drupal\Core\Config\ImmutableConfig|\Prophecy\Prophecy\ProphecyInterface $config */
+    $config = $this->prophesize(ImmutableConfig::class);
+    $config_factory->get('potion.gettext.settings')
+      ->willReturn($config->reveal());
+    $config->get('path')
+      ->willReturn('');
+
+    /** @var \Drupal\Potion\GettextWrapper $gettext_wrapper */
+    $gettext_wrapper = new GettextWrapper($config_factory->reveal());
+
     /** @var \Drupal\Core\Language\LanguageManagerInterface|\Prophecy\Prophecy\ProphecyInterface $language_manager */
     $language_manager = $this->prophesize(LanguageManagerInterface::class);
-    $this->utility = new Utility($language_manager->reveal());
+
+    $this->utility = new Utility($language_manager->reveal(), $gettext_wrapper);
     $language_manager->getLanguages()
       ->willReturn(['en' => $en, 'fr' => $fr]);
   }
